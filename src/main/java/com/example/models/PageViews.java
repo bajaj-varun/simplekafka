1062b65bae000d5ff981a982124c7308a7301767
Varun <bajaj.varun@hotmail.com>
2019-03-16 22:46:44 +0530

    Stream join example updated and created read.md file.

--- src/main/java/com/example/models/PageViews.java
+++ src/main/java/com/example/models/PageViews.java
@@ -1,49 +1,66 @@
 package com.example.models;
 
-import com.example.PageViewsValues;
-import com.example.pojos.PageViewsPojo;
-import com.example.serdes.JsonPojoDeserializer;
-import com.example.serdes.JsonPojoSerializer;
+import io.confluent.kafka.streams.serdes.avro.GenericAvroSerde;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import io.confluent.ksql.avro_schemas.KsqlDataSourceSchema;
+import org.apache.avro.generic.GenericRecord;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.Deserializer;
+import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.common.serialization.Serializer;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.Printed;
+import org.apache.kafka.streams.kstream.*;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 public class PageViews {
+
     public static void main(String[] args) {
-        final String TOPIC = "pageviews1";
+        final String TOPIC = "pageviews";
+        final String USERS_TOPIC = "users";
+
         final StreamsBuilder builder = new StreamsBuilder();
 
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "Test");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        //props.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, "1000");
-        //props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
-        Map<String, Object> serdeProps = new HashMap<>();
+        final Map<String, String> serdeConfig = new HashMap();
+        serdeConfig.put("schema.registry.url", "http://localhost:8081");
 
-        final Serializer<PageViewsPojo> serializer = new JsonPojoSerializer<>();
-        serdeProps.put("JsonPOJOClass",PageViewsPojo.class);
-        serializer.configure(serdeProps, false);
+        final Serde<String> keySpecificAvroSerde = new Serdes.StringSerde();
+        keySpecificAvroSerde.configure(serdeConfig, true);
 
-        final Deserializer<PageViewsPojo> deserializer = new JsonPojoDeserializer<>();
-        serdeProps.put("JsonPOJOClass",PageViewsPojo.class);
-        deserializer.configure(serdeProps, false);
+        final Serde<KsqlDataSourceSchema> valueSpecificAvroSerde = new SpecificAvroSerde<KsqlDataSourceSchema>();
+        valueSpecificAvroSerde.configure(serdeConfig, false);
 
-        final KStream<String, PageViewsPojo> stream = builder.stream(TOPIC, Consumed.with(Serdes.String(), Serdes.serdeFrom(serializer,deserializer)));
+        final KStream<String, KsqlDataSourceSchema> stream = builder.stream(TOPIC, Consumed.with(keySpecificAvroSerde, valueSpecificAvroSerde));
 
-        stream.print(Printed.toSysOut());
-        stream.foreach((k,v) -> System.out.println("Key =>"+k+", Value =>"+v.toString()));
+        // Users Serde
+        final Serde<GenericRecord> usersSerde = new GenericAvroSerde();
+        usersSerde.configure(serdeConfig, false);
+
+        final KStream<String, GenericRecord> userStream = builder.stream(USERS_TOPIC, Consumed.with(Serdes.String(),usersSerde));
+
+        //stream.print(Printed.toSysOut());
+        //stream.foreach((k,v) -> System.out.println("Key =>"+k.toString()+", v=>"+v.toString()));
+        //userStream.foreach((k,v) -> System.out.println("Key =>"+k.toString()+", v=>"+v.toString()));
+
+        stream.join(userStream,
+                (k,v)->"K=>"+k+", v=>"+v.toString(),
+                JoinWindows.of(TimeUnit.SECONDS.toMillis(10000)),
+                Joined.with(
+                        Serdes.String(),
+                        valueSpecificAvroSerde,
+                        usersSerde
+                )
+        ).print(Printed.toSysOut());
 
         KafkaStreams streams = new KafkaStreams(builder.build(),props);
         streams.start();
---
+++ src/main/java/com/example/models/read.md
@@ -0,0 +1,69 @@
+##**Stream Join example**
+
+For this example using Ksql-datagen tool to generate sample data.
+
+*Generate users data -*
+
+```
+ksql-datagen bootstrap-server=localhost:9092 quickstart=users format=avro topic=users key=userid maxInterval=600
+```
+*Generate pageviews data -*
+
+```
+ksql-datagen bootstrap-server=localhost:9092 quickstart=pageviews format=avro topic=pageviews key=userid maxInterval=600
+```
+
+*Image of sample data generation*
+
+![Sample data](../../../../../../images/dataGen.png)
+
+*Pageviews schema*
+```json
+{
+    "type": "record",
+    "name": "KsqlDataSourceSchema",
+    "namespace": "io.confluent.ksql.avro_schemas",
+    "fields": [{
+        "name": "viewtime",
+        "type": ["null", "long"],
+        "default": null
+    }, {
+        "name": "userid",
+        "type": ["null", "string"],
+        "default": null
+    }, {
+        "name": "pageid",
+        "type": ["null", "string"],
+        "default": null
+    }]
+}
+```
+
+*users schema*
+```json
+{
+	"type": "record",
+	"name": "KsqlDataSourceSchema",
+	"namespace": "io.confluent.ksql.avro_schemas",
+	"fields": [{
+		"name": "registertime",
+		"type": ["null", "long"],
+		"default": null
+	}, {
+		"name": "userid",
+		"type": ["null", "string"],
+		"default": null
+	}, {
+		"name": "regionid",
+		"type": ["null", "string"],
+		"default": null
+	}, {
+		"name": "gender",
+		"type": ["null", "string"],
+		"default": null
+	}]
+}
+```
+
+*Stream join*
+![Stream join](../../../../../../images/StreamJoin.PNG)
